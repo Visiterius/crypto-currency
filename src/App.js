b333d595fcdbc38c91a8e708a96ab12f112fc7df
Evgeny Bednyakov <30568068+Visiterius@users.noreply.github.com>
2022-08-01 18:14:00 +0300

    first comm

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import DisplayCurrency from "./components/DisplayCurrency";
+import './App.css'
+import logo from './assets/logo.svg'
+import icon from './assets/icon.svg'
+import figure from './assets/figure.png'
 
-function App() {
+export default function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+          <div className="content">
+              <img src={logo} alt="logo"/>
+              <h1>Now you can track<br/>
+                  all your cryptos here!</h1>
+              <h2>Just enter the <br/>
+                  cryptocurrency code on the <br/>
+                  from the right.
+              </h2>
+          </div>
+          <DisplayCurrency image={icon}/>
+      </div>
   );
 }
-
-export default App;
--- src/components/DisplayCurrency.js
+++ src/components/DisplayCurrency.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React,{useRef, useState} from 'react';
 import {gql, useQuery} from "@apollo/client";
-import {useRef, useState} from "react";
+import './DisplayCurrency.css'
 
+const DisplayCurrency = (props) => {
 
-const DisplayLocations = (props) => {
     const inputRef = useRef("BTC")
     const [currency,setCurrency]=useState("BTC")
+    const [price,setPrice]=useState([])
+    const [name,setName]=useState([])
 
     function handleClick(){
         setCurrency(inputRef.current.value)
+        setPrice(storage)
+        setName(cryptoName)
     }
-
 
     const GET_PRICES = gql`
         query price {
@@ -27,14 +30,58 @@ const DisplayLocations = (props) => {
     if (loading) return <p>Loading...</p>;
     if (error) return <p>Error :(</p>;
 
+    let test = JSON.stringify(Object.values(data.markets.map(({ticker})=>JSON.stringify(ticker)
+        .split(/[^.0-9]/))
+        .toString()
+        .split(/[^.0-9]/)
+        .filter(el=>el !== '')))
+        .slice(2).split('')
+        .reverse()
+        .slice(2)
+        .reverse()
+        .join('')
+
+
+    let storage = []
+
+    let cryptoList = Object.values(JSON.stringify(price).split(/[^,.0-9]/).filter(el=>el!==''&&el!==','))
+
+    let cryptoName = Object.values(JSON.stringify(name).split(/[^,.0-9]/).filter(el=>el!==''&&el!==','))
+
+
+
+    if (handleClick){
+        storage.push(cryptoList)
+        storage.push(test)
+        cryptoName.push(currency)
+        cryptoName.push(name)
+    }
+
+    console.log(JSON.stringify(cryptoName).split(/[^a-zA-Z]/).filter(el=>el!=='').reverse())
+
     return data.markets.map(({ticker}) => (
         <div>
-            <input type="text" ref={inputRef}/>
-            <button onClick={handleClick}>add currency</button>
-            <h1>{currency}</h1>
-            <h3>{JSON.stringify(ticker)}</h3>
+            <div className='form-box'>
+                <input className='input-curr' type="text" ref={inputRef}/>
+                <button className='add-btn' onClick={handleClick}>Add</button>
+                <label>Use of this service is subject to terms and<br/> conditions</label>
+            </div>
+            <br/>
+            <div className='list-item'>{JSON.stringify(storage)
+                .split(/[^.0-9]/)
+                .filter(el=>el!=='')
+                .flat()
+                .map((el,i)=>(
+                <div className='item'>
+                    <div className='item-info'>
+                        <div className='name'>{JSON.stringify(cryptoName).split(/[^a-zA-Z]/).filter(el=>el!=='').reverse()[i]}</div>
+                        <div className='num'>{parseFloat(el)}<label>€</label><hr/></div>
+                    </div>
+                    <img src={props.image} alt="icon"/>
+                </div>))}
+            </div>
         </div>
     ));
 };
 
-export default DisplayLocations;
+export default DisplayCurrency;
